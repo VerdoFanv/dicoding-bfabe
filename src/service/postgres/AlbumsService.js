9d9d14d18af765d8d183d0d5e9d9233c67d631fe
Verdo Fanv <86637675+VerdoFanv@users.noreply.github.com>
2022-09-21 21:08:44 +0700

    update req proyek 2

---
+++ src/service/postgres/AlbumsService.js
@@ -0,0 +1,87 @@
+const { Pool } = require('pg')
+const { nanoid } = require('nanoid')
+const InvariantError = require('../../error/InvariantError')
+const NotFoundError = require('../../error/NotFoundError')
+
+class AlbumsService {
+  constructor() {
+    this._pool = new Pool()
+  }
+
+  async addAlbum({ name, year }) {
+    const id = nanoid(16)
+    const query = {
+      text: 'INSERT INTO albums VALUES ($1, $2, $3) RETURNING id',
+      values: [`album-${id}`, name, year],
+    }
+
+    const result = await this._pool.query(query)
+    if (!result.rows[0].id) {
+      throw new InvariantError('Failed to added album')
+    }
+
+    return result.rows[0].id
+  }
+
+  async getAlbums() {
+    const result = this._pool.query('SELECT * FROM albums WHERE title LIKE ')
+    return result.rows
+  }
+
+  async getAlbumById(id) {
+    const query = {
+      text: 'SELECT * FROM albums WHERE id = $1',
+      values: [id],
+    }
+
+    const result = await this._pool.query(query)
+    if (!result.rowCount) {
+      throw new NotFoundError('Cannot find album ID!')
+    }
+
+    return result.rows[0]
+  }
+
+  async getAlbumByIdWithSongs(id) {
+    const query = {
+      text: `
+        SELECT songs.id, songs.title, songs.performer FROM albums
+        LEFT JOIN songs ON songs.album_id = albums.id
+        WHERE albums.id = $1`,
+      values: [id],
+    }
+
+    const result = await this._pool.query(query)
+    if (!result.rowCount) {
+      throw new NotFoundError('Cannot find album ID!')
+    }
+
+    return result.rows
+  }
+
+  async editAlbumById(id, { name, year }) {
+    const query = {
+      text: 'UPDATE albums SET name = $1, year = $2 WHERE id = $3',
+      values: [name, year, id],
+    }
+
+    const result = await this._pool.query(query)
+    if (!result.rowCount) {
+      throw new NotFoundError('Cannot find album ID!')
+    }
+  }
+
+  async deleteAlbumById(id) {
+    const query = {
+      text: 'DELETE FROM albums WHERE id = $1',
+      values: [id],
+    }
+
+    const result = await this._pool.query(query)
+    if (!result.rowCount) {
+      throw new NotFoundError('Cannot find album ID!')
+    }
+  }
+}
+
+module.exports = AlbumsService
